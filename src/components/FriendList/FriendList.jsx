0eb410543b879555042c57d4a268cb914a405e24
Olga-Melnyk <3717129@ukr.net>
2023-02-16 17:28:22 +0200

    add components files

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,20 @@
+import { FriendListItem } from '../FriendListItem/FriendListItem';
+import PropTypes from 'prop-types';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul class="friend-list">
+      {friends.map(friend => (
+        <FriendListItem key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+export const FriendListItem = ({ friend: { avatar, name } }) => {
+  return (
+    <li class="item">
+      <span class="status"></span>
+      <img class="avatar" src={avatar} alt={name} width="48" />
+      <p class="name">{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  friend: PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
